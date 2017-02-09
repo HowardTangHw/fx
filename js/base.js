9d00dbde16ba8a0aa94517e577d681abefcf853c
HowardTangHw <2379034@qq.com>
2017-02-09 19:11:56 +0800

    2017年2月9日19:11:55

--- js/base.js
+++ js/base.js
@@ -8,8 +8,8 @@ window.onload = function() {
     var qqcall = document.getElementsByClassName("qqcall");
     for (var i = 0; i < qqcall.length; i++) {
         if (winWidth < 768) {
-            qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="mqqwpa://im/chat?chat_type=wpa&uin=241639308&version=1&src_type=web&web_src=bjhuli.com" role="button">马上联系我们!</a>';
-            qqcall[0].innerHTML = '<a target="_blank" class="nav-call" href="mqqwpa://im/chat?chat_type=wpa&uin=241639308&version=1&src_type=web&web_src=bjhuli.com" role="button">马上联系我们!</a>';
+            qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="tel:13824637212" role="button">马上联系我们!</a>';
+            qqcall[0].innerHTML = '<a target="_blank" class="nav-call" href="tel:13824637212" role="button">马上联系我们!</a>';
         } else {
             qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="http://wpa.qq.com/msgrd?v=3&uin=241639308&site=qq&menu=yes" role="button">马上联系我们!</a>';
             qqcall[0].innerHTML = '<a target="_blank" class="nav-call" href="http://wpa.qq.com/msgrd?v=3&uin=241639308&site=qq&menu=yes" role="button">马上联系我们!</a>';
