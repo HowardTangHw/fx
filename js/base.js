9a1b753aa104e39f7ceeb1cfa25b0d73db8e2c03
HowardTangHw <2379034@qq.com>
2017-02-13 17:20:00 +0800

    2017年2月13日17:19:58

--- js/base.js
+++ js/base.js
@@ -8,11 +8,9 @@ window.onload = function() {
     var qqcall = document.getElementsByClassName("qqcall");
     for (var i = 0; i < qqcall.length; i++) {
         if (winWidth < 768) {
-            qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="tel:13824637212" role="button">马上联系我们!</a>';
-            qqcall[0].innerHTML = '<a target="_blank" class="nav-call" href="tel:13824637212" role="button">马上联系我们!</a>';
+            qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="tel:13824637212" role="button"><span class="glyphicon glyphicon-earphone"></span>马上联系我们!</a>';
         } else {
-            qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="http://wpa.qq.com/msgrd?v=3&uin=241639308&site=qq&menu=yes" role="button">马上联系我们!</a>';
-            qqcall[0].innerHTML = '<a target="_blank" class="nav-call" href="http://wpa.qq.com/msgrd?v=3&uin=241639308&site=qq&menu=yes" role="button">马上联系我们!</a>';
+            qqcall[1].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="http://wpa.qq.com/msgrd?v=3&uin=241639308&site=qq&menu=yes" role="button"><span class="glyphicon glyphicon-earphone"></span>马上联系我们!</a>';
         }
     }
 
--- js/introduce.js
+++ js/introduce.js
@@ -2,7 +2,6 @@ window.onload = function() {
     var f = document.getElementById("introduce-tab-btn");
     var btn = f.children;
     var div = document.getElementsByClassName("content-border");
-    console.log(div);
     for (var i = 0; i < btn.length; i++) {
         btn[i].index = i;
         btn[i].onclick = function() {
@@ -12,6 +11,21 @@ window.onload = function() {
             }
             div[this.index].style.display = "block";
             btn[this.index].className = "col-sm-6 col-xs-6 active";
+
+        }
+    }
+    var winWidth = "";
+    if (window.innerWidth) {
+        winWidth = window.innerWidth;
+    } else if ((document.body) && (document.body.clientWidth)) {
+        winWidth = document.body.clientWidth;
+    }
+    var qqcall = document.getElementsByClassName("qqcall");
+    for (var i = 0; i < qqcall.length; i++) {
+        if (winWidth < 768) {
+            qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="tel:13824637212" role="button"><span class="glyphicon glyphicon-earphone"></span>马上联系我们!</a>';
+        } else {
+            qqcall[1].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="http://wpa.qq.com/msgrd?v=3&uin=241639308&site=qq&menu=yes" role="button"><span class="glyphicon glyphicon-earphone"></span>马上联系我们!</a>';
 
         }
     }
