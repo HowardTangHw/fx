b8b671ebdcf2e2e68a06e58fb2935906ef63666e
HowardTangHw <2379034@qq.com>
2017-02-14 17:10:43 +0800

    2017年2月14日17:10:41

--- js/base.js
+++ js/base.js
@@ -8,7 +8,7 @@ window.onload = function() {
     var qqcall = document.getElementsByClassName("qqcall");
     for (var i = 0; i < qqcall.length; i++) {
         if (winWidth < 768) {
-            qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="tel:13824637212" role="button"><span class="glyphicon glyphicon-earphone"></span>马上联系我们!</a>';
+            qqcall[i].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="tel:13824633523" role="button"><span class="glyphicon glyphicon-earphone"></span>马上联系我们!</a>';
         } else {
             qqcall[1].innerHTML = '<a target="_blank" class="btn btn-lg btn-page-banner" href="http://wpa.qq.com/msgrd?v=3&uin=241639308&site=qq&menu=yes" role="button"><span class="glyphicon glyphicon-earphone"></span>马上联系我们!</a>';
         }
